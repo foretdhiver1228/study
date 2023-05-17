9a8c2d949c9bb1378fc65f8ef6de3c49285a33eb
foretdhiver1228 <zwielichter@gmail.com>
2023-05-17 22:05:43 +0900

    spring boot study

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,7 +9,9 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!!");
+        model.addAttribute("data", "spring!!!");
         return "hello";
     }
+
+    // 해야할 일 : 부트 기초 공부. 스프링 공부. 부트로 프로젝트 하나 만들기.
 }
