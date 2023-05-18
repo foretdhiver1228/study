c822cf18a85599e9ecaf54dac8f27b90f70f5b96
foretdhiver1228 <zwielichter@gmail.com>
2023-05-18 18:55:07 +0900

    spring boot study

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,5 +15,35 @@ public class HelloController {
         return "hello";
     }
 
-    // 해야할 일 : 부트 기초 공부. 스프링 공부. 부트로 프로젝트 하나 만들기.
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        // value 에 command + p
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // body 부에 이 내용을직접 넣어주겠다 라는 어노테이션 = responseBody
+    }
+
+    @GetMapping("hello-api") // api타입이라 말하는 그것
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // JSON방식으로 리턴됨 key : value 구조.
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() { // 프로퍼티 접근방식이라고도 함 getter setter를...
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
 }
