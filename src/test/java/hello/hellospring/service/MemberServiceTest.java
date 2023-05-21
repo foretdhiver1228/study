59381e6a1eeb031c02d13c0f0d070a1b8e6f3bd6
foretdhiver1228 <zwielichter@gmail.com>
2023-05-21 20:47:25 +0900

    spring boot study

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -51,17 +51,13 @@ class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-/*        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
-        }*/
 
-        //then
     }
 
     @Test
