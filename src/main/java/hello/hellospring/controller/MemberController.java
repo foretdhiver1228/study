a68c44d8a60c06151905006f7aa6009a8fe231c2
foretdhiver1228 <zwielichter@gmail.com>
2023-05-22 13:56:19 +0900

    new controller added

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,10 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
