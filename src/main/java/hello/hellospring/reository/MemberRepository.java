bba73b1ac8c92e961e10b36e7aadf858da8db333
foretdhiver1228 <zwielichter@gmail.com>
2023-05-25 10:08:59 +0900

    update Controller

--- src/main/java/hello/hellospring/reository/MemberRepository.java
+++ src/main/java/hello/hellospring/reository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.reository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.List;
 
+@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
