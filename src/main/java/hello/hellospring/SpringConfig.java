7d8d02dc61edafe90257200d6d86138c76fad1e3
foretdhiver1228 <zwielichter@gmail.com>
2023-06-01 21:06:31 +0900

    MemberController.java and other 8 changed

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,16 +1,17 @@
 package hello.hellospring;
-
+import hello.hellospring.reository.MemberRepository;
+import hello.hellospring.reository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- src/main/java/hello/hellospring/reository/MemberRepository.java
+++ src/main/java/hello/hellospring/reository/MemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.List;
 
-@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
